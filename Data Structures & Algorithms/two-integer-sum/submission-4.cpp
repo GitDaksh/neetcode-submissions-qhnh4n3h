c306a9984f5be6e7f9f834ade27526ab84b6abfa
Daksh <daksh.java.util@gmail.com>
2026-04-07 17:44:17 +0530

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map;
+
+        for(int i = 0 ; i < nums.size() ; i++) {
+            int rem = target - nums[i];
+
+            if(map.count(rem)) {
+                return {map[rem], i};
+            }
+
+            map[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
